ffad1b384fe439ebc00d920190a8f812aaca9b38
ibkrob <102308006+ibkrob@users.noreply.github.com>
2022-04-28 17:31:47 +0200

    karte mit startlayer u stephansdom

--- main.js
+++ main.js
@@ -4,5 +4,20 @@ let stephansdom = {
 lat: 48.208943,
 lng: 16.373118,
 title: "Stephansdom"
+};
 
-}
+let startLayer = L.tileLayer.provider("BasemapAT.basemap");
+
+let map = L.map("map", {
+    center: [stephansdom.lat, stephansdom.lng],
+    zoom: 12,
+    layers:[
+        startLayer
+    ]
+
+})
+
+let layerControl = L.control.layers(
+    {
+        "BasemapAT.basemap": startLayer,
+    }).addTo(map)
