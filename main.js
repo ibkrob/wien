a67e4d1ce6b684ab10f8147bdc902f76ca7ef89e
ibkrob <102308006+ibkrob@users.noreply.github.com>
2022-04-28 17:43:22 +0200

    layer hinzu

--- main.js
+++ main.js
@@ -18,6 +18,11 @@ let map = L.map("map", {
 })
 
 let layerControl = L.control.layers(
-    {
-        "BasemapAT.basemap": startLayer,
-    }).addTo(map)
+    {   "BasemapAT Standard": startLayer,
+        "BasemapAT Grau": L.tileLayer.provider("BasemapAT.grau"),
+        "BasemapAT Overlay": L.tileLayer.provider("BasemapAT.overlay"),
+        "BasemapAT Terrain": L.tileLayer.provider("BasemapAT.terrain"),
+        "BasemapAT Surface": L.tileLayer.provider("BasemapAT.surface"),
+        "BasemapAT High DPI": L.tileLayer.provider("BasemapAT.highdpi"),
+        "BasemapAT Ortophoto": L.tileLayer.provider("BasemapAT.orthofoto")
+    }).addTo(map);
