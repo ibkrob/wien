627a735944c477ad83d8416eb8d81d81bf1528c8
ibkrob <102308006+ibkrob@users.noreply.github.com>
2022-05-05 16:21:26 +0200

    stops hinzu

--- main.js
+++ main.js
@@ -66,4 +66,23 @@ async function loadSites(url) {
 }
 
 
-loadSites("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json")
+loadSites("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json");
+
+//Sights
+
+async function loadStops(url) {
+    let response = await fetch(url);
+    let geojson = await response.json();
+    //console.log(geojson);
+    
+    let overlay = L.featureGroup();
+    layerControl.addOverlay(overlay, "Stops");
+    overlay.addTo(map);
+
+    L.geoJSON(geojson).addTo(overlay);
+
+}
+
+
+loadSites("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKHTSVSLOGD&srsName=EPSG:4326&outputFormat=json");
+
